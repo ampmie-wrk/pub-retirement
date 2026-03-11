dc0622dd3cb0b39df0cff9c000e86e3a54d81769
ampmie-wrk <ampmie.wrk@gmail.com>
2026-03-12 01:47:53 +0700

    Modified config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react({
+      base: "/pub-retirement/",
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
